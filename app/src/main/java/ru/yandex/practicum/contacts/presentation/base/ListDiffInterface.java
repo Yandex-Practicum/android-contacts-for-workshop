35103f7d877ee4a730cb45b6510562d32c780ddc
Cunickal <egorkamasharov@gmail.com>
2025-01-26 17:36:36 +0300

    моя вторая проектная работа(исправленная) #2

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -2,5 +2,9 @@ package ru.yandex.practicum.contacts.presentation.base;
 
 public interface ListDiffInterface<T> {
     boolean theSameAs(T newItem);
+    @Override
+    boolean equals(Object obj);
 }
+
+
 
